9c7152cc6f6615aab9697bfa14278140b26dc674
Lakshay Raj <lakshaywork012@gmail.com>
2026-08-16 23:46:07 +0530

    Digital clock js

--- Digital_clock/script.js
+++ Digital_clock/script.js
@@ -1,5 +1,25 @@
-let hrs = document.getElementById("hrs");
-let mins = document.getElementById("mins");
-let secs = document.getElementById("secs");
+let hrs = document.getElementById("hours");
+let mins = document.getElementById("minutes");
+let secs = document.getElementById("seconds");
+
+let currentTime = new Date();
+let currentHours = currentTime.getHours(); // Get the current hours
+let currentMinutes = currentTime.getMinutes(); // Get the current minutes
+let currentSeconds = currentTime.getSeconds(); // Get the current seconds
+
+hrs.innerHTML = currentHours;
+mins.innerHTML = currentMinutes;
+secs.innerHTML = currentSeconds;
+
+setInterval(() => { // Update the time every second
+  let newTime = new Date();
+  let newHours = newTime.getHours();
+  let newMinutes = newTime.getMinutes();
+  let newSeconds = newTime.getSeconds();
+
+  hrs.innerHTML = newHours;
+  mins.innerHTML = newMinutes;
+  secs.innerHTML = newSeconds;
+}, 1000);
 
 
